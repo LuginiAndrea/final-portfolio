a4b265c40eb1f31c9c64130cf50df86576bf7dc6
Andrea Lugini <lugiolugini@gmail.com>
2022-02-09 17:15:55 +0100

    siummico

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -25,8 +25,15 @@
                 <button class="change-page-button" on:click={() => changePage(i)}>{option.title}</button>
             {/if}
         {/each}
-    </ul>    
+    </ul>   
 </main>
+    <footer>
+        <ul>
+            <li><img src="./img/light-github.png" alt="github logo"></li>
+            <li><img src="./img/light-github.png" alt="github logo"></li>
+            <li><img src="./img/light-github.png" alt="github logo"></li>
+        </ul>
+    </footer> 
 
 <style>
     .change-page-button {
@@ -43,4 +50,77 @@
         width: 20%;
         margin: auto;
     }
+
+    :global(html) {
+        background-color: #222222;
+    }
+
+    :global(body) {
+        margin: 0;
+        padding: 0;
+        display:flex; 
+        flex-direction:column; 
+    }
+
+    main {
+        color: #f8f8e8;
+        font-family:'Courier New', Courier, monospace;
+        margin: 50px 10px 0px 10px;
+    }
+
+    h1 {
+        font-weight: 500;
+        text-align: center;
+        font-size: 8.5em;
+        margin: auto;
+        transition: ease all .5s;
+    }
+
+    @media(max-width: 550px) and (min-width: 500px) {
+        h1 {
+            font-size: 6.5em;
+        }
+        :global(p) {
+            width: 70%;
+        }
+    }
+
+    @media(max-width: 500px) {
+        h1 {
+            font-size: 5.5em;
+        }
+    }
+
+    :global(p) {
+        width: 80%;
+        margin: auto;
+        transition: ease all .5s;
+    }
+
+    footer {
+        margin-top:auto; 
+        background-color: black;
+        width: 100%;
+        text-align: center;
+        font-size: 2em;
+        border-top: 2px solid white;
+    }
+
+    footer ul {
+        list-style-type: none;
+        padding: 0;
+        margin: 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
+    footer {
+        padding: 8px 0px 0px 0px;
+    }
+    footer img {
+        width: 50px;
+        height: 50px;
+    }
+
 </style>
